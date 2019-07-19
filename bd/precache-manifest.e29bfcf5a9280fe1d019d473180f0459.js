4da91daa6e4808e4861ab2639efcee5a5c41f396
jxq <651830202@qq.com>
2019-07-19 15:05:19 +0800

    fix

---
+++ bd/precache-manifest.e29bfcf5a9280fe1d019d473180f0459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5a32e31ebf8a5a5f9eb33cff53403f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe8aeca5a7e423963538",
+    "url": "./static/css/2.6df101df.chunk.css"
+  },
+  {
+    "revision": "67436539646301ddc0c1",
+    "url": "./static/css/main.d0328dd8.chunk.css"
+  },
+  {
+    "revision": "fe8aeca5a7e423963538",
+    "url": "./static/js/2.dd994cdd.chunk.js"
+  },
+  {
+    "revision": "67436539646301ddc0c1",
+    "url": "./static/js/main.ba36ba03.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
